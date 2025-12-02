491335ba6bd58b450796adac52608711395a2454
nikita <nikita.kashkin@student.put.poznan.pl>
2025-12-02 15:58:21 +0100

    Zadanie 2.3 (str. 6)

--- Lab8/Lab8.cpp
+++ Lab8/Lab8.cpp
@@ -1,12 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+/*
+void swap(int* a, int* b);
 
+int main() {//zadanie 2.3 (strona 6)
+	int a = 10;
+	int b = 20;
+	cout << a << endl;
+	cout << b << endl;
+	swap(&a, &b);
+	cout << a << endl;
+	cout << b << endl;
+
+	return 0;
+}
+
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+*/
+
+/*
+int main() {//zadanie 2
+
+	int jakas_tablica[10];
+	int *wsk;
+	wsk = &jakas_tablica[0];
+	//wsk = jakas_tablica; //te dwie linijki sa rownowazne
+	for (int i = 0; i < 10; i++) {
+		cout << "Wartosc " << i+1 << " elementu tablicy jest rowna: " << *wsk <<endl;
+		wsk++;
+	}
+
+	wsk -= 2;
+	*wsk = 77;
+	wsk = &jakas_tablica[0];
+
+	for (int i = 0; i < 10; i++) {
+		cout << "Wartosc " << i + 1 << " elementu tablicy jest rowna: " << *wsk << endl;
+		wsk++;
+	}
 
 
 	return 0;
 }
+*/
 
 /*
 int main() {//zadanie 2.1 , 2.2 (strona 5)
